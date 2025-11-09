153cad5a6e0e0f996666d475ad8650602363b12a
Swayamjit Biswal <swoyamjitbiswal@gmail.com>
2025-11-09 09:25:57 +0530

    updated ReviewListConverter.kt

--- app/src/main/java/com/example/shopease/data/ReviewListConverter.kt
+++ app/src/main/java/com/example/shopease/data/ReviewListConverter.kt
@@ -6,14 +6,13 @@ import com.google.gson.reflect.TypeToken
 
 class ReviewListConverter {
     @TypeConverter
-    fun fromString(value: String): List<Review> {
+    fun fromReviewList(value: String?): List<Review>? {
         val listType = object : TypeToken<List<Review>>() {}.type
         return Gson().fromJson(value, listType)
     }
 
     @TypeConverter
-    fun fromList(list: List<Review>): String {
-        val gson = Gson()
-        return gson.toJson(list)
+    fun toReviewList(list: List<Review>?): String? {
+        return Gson().toJson(list)
     }
 }
