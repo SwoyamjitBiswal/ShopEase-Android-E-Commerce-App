44b0e6a70b3137f36f468e6d056131a8cc5e5694
Swayamjit Biswal <swoyamjitbiswal@gmail.com>
2025-11-09 09:07:16 +0530

    updated Converters.kt

---
+++ app/src/main/java/com/example/shopease/data/Converters.kt
@@ -0,0 +1,29 @@
+package com.example.shopease.data
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.util.Date
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+
+    @TypeConverter
+    fun fromCartItemList(value: String?): List<CartItem>? {
+        val listType = object : TypeToken<List<CartItem>>() {}.type
+        return Gson().fromJson(value, listType)
+    }
+
+    @TypeConverter
+    fun toCartItemList(list: List<CartItem>?): String? {
+        return Gson().toJson(list)
+    }
+}
