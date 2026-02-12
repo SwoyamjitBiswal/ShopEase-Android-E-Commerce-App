a73ac35a8b5a2d17c62b27d66d607b89d56cea0e
Swayamjit Biswal <swoyamjitbiswal@gmail.com>
2026-02-12 10:00:21 +0530

    updated

--- app/src/main/java/com/example/shopease/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/shopease/viewmodel/HomeViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.shopease.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.example.shopease.data.Product
+import com.example.shopease.data.HomeItem
 import com.example.shopease.data.ProductRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class HomeViewModel(private val repository: ProductRepository) : ViewModel() {
 
-    val products: Flow<List<Product>> = repository.getAllProducts()
+    val homeItems: Flow<List<HomeItem>> = repository.getAllProducts().map {
+        val items = mutableListOf<HomeItem>()
+        items.add(HomeItem.HeaderItem("Featured Products"))
+        items.addAll(it.map { product -> HomeItem.ProductItem(product) })
+        items
+    }
 }
